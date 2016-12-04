67c9b98590125f727dc52c418d8a770d339d7b3a
Daria <oruchuban37@gmail.com>
2016-12-04 19:09:14 +0100

    Moving objects+falling

---
+++ GameOne/Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour {
+	public float speed=.2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x>=30)
+		transform.Translate(-speed,0,0);
+		else
+		{
+			transform.Translate(40,0,0);
+				}
+	}
+}
